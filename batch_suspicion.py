bc200d669ab3eeaa59b01c213c8ab0c0dba1ff36
freire-guido <14301656+freire-guido@users.noreply.github.com>
2025-12-14 20:13:54 -0300

    flexible batch run

---
+++ batch_suspicion.py
@@ -0,0 +1,66 @@
+from batchutils import run_batch_with_tracker, generate_batch_request
+
+from concurrent.futures import ThreadPoolExecutor
+from dotenv import load_dotenv
+from openai import OpenAI
+import json
+
+load_dotenv()
+client = OpenAI()
+
+def format_messages_prompt(
+    prompt: dict,
+    format_vars: dict,
+) -> dict:
+    return {
+        "role": prompt['role'],
+        "content": prompt['content'].format(**format_vars)
+    }
+
+def main(args):
+    with open(args.conversations, 'r') as f:
+        conversations = [json.loads(line) for line in f]
+    with open(args.blue_prompts, 'r') as f:
+        blue_prompts = json.load(f)
+    with open(args.red_prompts, 'r') as f:
+        red_prompts = json.load(f)
+    with open(args.honest_prompt, 'r') as f:
+        honest_prompt = json.load(f)
+    with open(args.tracker, 'r') as f:
+        tracker = json.load(f)
+
+
+    with ThreadPoolExecutor(max_workers=args.n_threads) as executor:
+        executor.submit(run_batch_with_tracker,
+            client,
+            [
+                generate_batch_request(
+                    custom_id=convo['prompt_id'],
+                    messages=[
+                        format_messages_prompt(prompt, {
+                            'conversation': convo['prompt'],
+                            'interactions': convo['interactions']
+                        }) for prompt in red_prompts[0]
+                    ],
+                    model=args.model
+                ) for convo in conversations
+            ],
+            args.out_dir + '/red_interactions.jsonl',
+            tracker,
+            'red_interactions_0',
+            verbose=True,
+        )
+        executor.submit(run_batch_with_tracker,
+            client,
+            [
+                generate_batch_request(
+                    custom_id=convo['prompt_id'],
+                    messages=red_prompts[1] + convo['prompt'],
+                    model=args.model
+                ) for convo in conversations
+            ],
+            args.out_dir + '/red_interactions.jsonl',
+            tracker,
+            'red_interactions_1',
+            verbose=True,
+        )
